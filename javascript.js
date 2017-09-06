084d961bd3f4372b5d7ca2deaac942bb19b5ebce
Dakota J Pfeifer <dakota.pfeifer@yahoo.com>
2017-09-06 11:25:44 -0600

    updating files

---
+++ javascript.js
@@ -0,0 +1,22 @@
+function submitInfo() {
+  var name = prompt("What's your name?")
+
+ if (name.length === 0) {
+    alert("You left this blank! WHO ARE YOU?!")
+  } else (name.length === null)
+    var email = prompt("What's your email address?")
+    {if (email.length === 0) {
+      alert("How the fuck are we supposed to contact you. Dumb shit.")
+    } else (email.length === null)
+      var inqury = prompt("What the fuck do you wanna say?")
+      {if (inqury.length === 0) {
+        alert("You don't wanna say anything? THEN WHAT WAS THE POINT OF THIS?!")
+      } else (inqury.length === null)
+
+      }
+    }
+  }
+
+
+
+submitInfo()
